557911e5a2a702365750c843dadfea7132607b2f
ThomG1 <thomas.gwyther@hotmail.co.uk>
2022-11-20 18:07:29 +0000

    Add correcting first test

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,7 +1,7 @@
+
 /**
  * @jest-environment jsdom
  */
-
 
  const { game } = require("../game");
 
@@ -13,9 +13,21 @@
      document.write(fileContents);
      document.close();
  });
-
- describe ("game object contains correct keys", () => {
-    test ("score exists", () => {
-        expect ("current game" in game).toBe(true);
-    });
+ 
+ describe("game object contains correct keys", () => {
+     test("score key exists", () => {
+         expect("score" in game).toBe(true);
+     });
+     test("currentGame key exists", () => {
+         expect("currentGame" in game).toBe(true);
+     });
+     test("playerMoves key exists", () => {
+         expect("playerMoves" in game).toBe(true);
+     });
+     test("choices key exists", () => {
+         expect("choices" in game).toBe(true);
+     });
+     test("choices contain correct ids", () => {
+         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+     });
  });
